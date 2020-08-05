6a57a12a899331a190776a22be653e1e790c1c7b
nerdfiles <nerdfiles@gmail.com>
2020-08-05 09:42:27 -0500

    Init

---
+++ index.js
@@ -0,0 +1,165 @@
+const doc = document;
+
+const createColorListContainer = () => {
+  const colorListContainer = document.createElement('ul');
+
+  colorListContainer
+    .setAttribute('aria-labelledby', 'palette');
+
+  colorListContainer
+    .setAttribute('class', 'palette__list no-margin no-padding');
+
+  colorListContainer
+    .setAttribute('role', 'radiogroup');
+
+  return colorListContainer;
+};
+
+
+const generatePalette = (config) => {
+
+  const {
+    colorList,
+    previewElem,
+    parent
+  } = config;
+
+  let inputElement;
+  let listElement;
+  let inputList = [];
+
+  let idForInputName = Math.ceil(Math.random() * 100000000);
+
+  for (var i = 0; i < colorList.length; i++) {
+
+    listElement=  document.createElement('li');
+    listElement
+      .setAttribute('class', 'list__color-item no-bullet no-margin');
+    listElement
+      .setAttribute('style', `background-color: ${colorList[i].hex}`)
+
+    inputElement = document.createElement('input');
+
+    inputElement
+      .setAttribute('class', 'color-item__input');
+    inputElement
+      .setAttribute('role', 'radio');
+    inputElement
+      .setAttribute('aria-checked', 'false');
+
+    if (i > 0) {
+      inputElement
+        .setAttribute('tabindex', '-1');
+    } else {
+      inputElement
+        .setAttribute('tabindex', '0');
+    }
+
+    inputElement
+      .setAttribute('value', colorList[i].hex);
+
+    inputElement
+      .setAttribute('type', 'radio');
+
+    inputElement
+      .setAttribute('id', `colorItemInput-${idForInputName}-${i}`);
+
+    inputElement
+      .setAttribute('name', `colorItemInput-${idForInputName}`);
+
+    inputElement.addEventListener('change', (e) => {
+      previewElem.setAttribute('style', `background-color: ${e.target.value}`);
+      const checkboxesForComponent = Array.from(parent.querySelectorAll('input'));
+      let checkbox;
+      for (var j = 0; j < checkboxesForComponent.length; j++) {
+        checkbox = checkboxesForComponent[j];
+        checkbox.setAttribute('aria-checked', 'false');
+      }
+      e.target.setAttribute('aria-checked', 'true');
+    });
+
+    listElement.appendChild(inputElement);
+
+    inputList.push(listElement);
+
+  }
+
+  return inputList;
+
+};
+
+const newPicker = (config) => {
+
+  const { selector, colorList } = config;
+
+  const component = document.querySelector(`${selector}`);
+  const colorPickerPreview = component.querySelector('.color-picker__preview');
+  const colorPickerPalette = component.querySelector('.color-picker__palette')
+
+  const paletteConfig = {
+    colorList: colorList,
+    previewElem: colorPickerPreview,
+    parent: component
+  };
+
+  const listContainer = createColorListContainer();
+  const listOfColors = generatePalette(paletteConfig);
+
+  listOfColors.forEach((elemRef) => {
+    listContainer.appendChild(elemRef);
+  });
+
+  colorPickerPalette.appendChild(listContainer);
+
+};
+
+const loadedListener = () => {
+
+  newPicker({
+    selector: '#picker1',
+    colorList: [
+      {
+        hex: '#FE2E64'
+      },
+      {
+        hex: '#81BEF7'
+      },
+      {
+        hex: '#01DF01'
+      },
+      {
+        hex: '#FFFF00'
+      },
+      {
+        hex: '#FA8258'
+      },
+      {
+        hex: '#cccccc'
+      }
+    ]
+  });
+
+  newPicker({
+    selector: '#picker2',
+    colorList: [
+      {
+        hex: '#FE2E64'
+      },
+      {
+        hex: '#81BEF7'
+      },
+      {
+        hex: '#01DF01'
+      },
+      {
+        hex: '#FFFF00'
+      },
+      {
+        hex: '#FA8258'
+      }
+    ]
+  });
+};
+
+doc.addEventListener('DOMContentLoaded', loadedListener);
+
